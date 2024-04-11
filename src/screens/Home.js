2c6694833497673d9467067a3884c345ec75cb3d
Barbizan2023 <viniciusbarbizann@gmail.com>
2024-04-11 13:03:39 -0300

    atualizado

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,49 +1,75 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, StatusBar, FlatList } from "react-native";
 import MusicItem from "../components/MusicItem";
+import { useSafeAreaFrame } from "react-native-safe-area-context";
+import { Audio } from "expo-av";
 
-export default function Home({navigation}) {
-  const [currentPlaying, setCurrentPlaying]= useState(null);
-
+export default function Home({ navigation }) {
+  const [currentPlaying, setCurrentPlaying] = useState(null);
+  const [musicData, setMusicData] = useState([]);
+  const [currentSound,setCurrentSound] = useState(null);
   const item = {
-    id: 1,
-    title:"MIGO ou VIDA",
-    group: "AC/DC", 
-    album_image:
-    "https://i1.sndcdn.com/artworks-Jkkb4nnOE3u0sqzK-YUWlBg-t500x500.jpg",
-    album: "Meu Nome Não é Igor",
-    year:2023,
-    genre: "Funk",
-  };
-   
-  
+    id: '1',
+    title: "Highway to hell",
+    group: "AC/DC",
+    album_image: "",
+    album: "Highway to hell",
+    year: 1970,
+    genre: "Heavy Metal",
+  }
 
+  const togglePlayPause = async (item) => {
+    if(currentSound && currentPlaying == item.id) {
+      await currentSound.pauseAsync();
+      setCurrentPlaying(null);
+      setCurrentSound(null);
+     } else{
+      if(currentSound){
+        await currentSound.unloadAsync();
+      }
+      const {sound} = await Audio.Sound.createAsync(
+        {uri: `http://10.0.2.2:3000/musics/${item.music_path}`},
+        {shouldPlay: true}
+      );
+      setCurrentSound(sound);
+      setCurrentPlaying(item.id);
+    }
+  }
+
+  useEffect(() => {
+    fetch("http://10.0.2.2:3000/musics")
+      .then((response) => response.json())
+      .then((data) => setMusicData(data));
+  }, []);
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#121212"/>
+      <StatusBar barStyle="light-content" backgroundColor="#121212" />
       <Text style={styles.title}>Minhas Músicas</Text>
-        <MusicItem 
-        isPlaying={() => currentPlaying == item.id} 
-        music={item} 
-        navigation={navigation} 
-        onPlayPause={() => {}}/>
+      <FlatList
+        data={musicData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => 
+          <MusicItem
+            isPlaying={ currentPlaying == item.id}
+            music={item}
+            navigation={navigation}
+            onPlayPause={() => togglePlayPause(item)}/>}/>
     </View>
   );
 }
 
-const styles= StyleSheet.create({
-  container:{
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
     backgroundColor: "#121212",
-    paddingTop:16,
-
-    },
-  title:{
-    fontSize:36,
-    fontWeight:"bold",
-    color:"#fff",
-    marginBottom:20,
-    marginLeft:20,
+    paddingTop: 16,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 20,
+    marginLeft: 20,
   },
 });
